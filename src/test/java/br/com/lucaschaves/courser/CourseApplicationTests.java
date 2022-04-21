fc776c3260f035301ce5d23505b6162d6e37fdd1
Lucas <luucas.chaves@gmail.com>
2022-04-21 16:37:58 -0300

    First Commit - Project Created

---
+++ src/test/java/br/com/lucaschaves/courser/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaschaves.courser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
