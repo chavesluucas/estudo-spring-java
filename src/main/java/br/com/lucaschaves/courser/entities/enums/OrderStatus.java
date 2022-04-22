d34ba698d6767ec35ccc73640529be02bcc4b355
Lucas <luucas.chaves@gmail.com>
2022-04-22 20:25:49 -0300

    Add OrderStatus ENUM

---
+++ src/main/java/br/com/lucaschaves/courser/entities/enums/OrderStatus.java
@@ -0,0 +1,32 @@
+package br.com.lucaschaves.courser.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMEND(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	
+	//informa um código para retornar uum valor (tipo, informa 2 e retorna PAID_
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+	
+}
